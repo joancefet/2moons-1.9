c365eb82bbd6bef35a2daa35ee6bc53ab0e6014c
HikeGame <mostier.kevin@gmail.com>
2017-10-03 00:39:41 +0200

    Initial
    Initial file and 4 mods

---
+++ README.md
@@ -0,0 +1,28 @@
+# 2moons 1.9
+New version of 2moons 1.9 with full mods integrate.
+
+## Information
+> **2Moons V 1.9** Is a game created with the base of 2moons 1.8 which will be edit is correct with additions made for this game.
+A support is of course at your disposal either by the forum of [2moons](http://2moons.de) or directly by github.
+
+## Development follow-up
++ Design
+  - standard
++ Bug Liste
+  - none 
++ Mods Lists
+  - Res Bonus
+  - Lucky
+  - Fleet Bonus
+  - Def Bonus
+
+## Developer
++ Project Leader
+  - Hike (Danter14)
++ Project Participant
+  - mimikri
++ Thanks Translate
+  - Person currently
+
+## Free Version
+> **Version 1.9**
---
+++ language/ru/CUSTOM.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ *  2Moons 
+ *   by Jan-Otto Kröpke 2009-2016
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * @package 2Moons
+ * @author Jan-Otto Kröpke <slaver7@gmail.com>
+ * @copyright 2009 Lucky
+ * @copyright 2016 Jan-Otto Kröpke <slaver7@gmail.com>
+ * @licence MIT
+ * @version 1.8.0
+ * @link https://github.com/jkroepke/2Moons
+ */
+
+// Translation into Russian - Copyright © 2010-2013 InquisitorEA <support@moon-hunt.ru>
+
+// Если у Вас есть собственные языковые переменные, Вы можете поместить их здесь. Файл не будет переписан системой обновления.
+// Также Вы можете переписать значение уже существующих языковых переменных. Для этого используйте тот же ключ.
+
+$LNG['extraone']     = 'Âû ìîæåòå êóïèòü îäíó íîâóþ ïëàíåòó ïîñëå òîãî, êàê âû çàñåëèëè %s ïëàíåò (ìàêñèìàëüíîå êîëè÷åñòâî ðàçðåøåííûõ ïëàíåò)';
+$LNG['extratwo'] = 'Âû êóïèëè';
+$LNG['extratree'] = '%s ïëàíåò';
+$LNG['extrafour'] = 'Âû äîëæíû îòïðàâèòü íîâóþ ìèññèþ êîëîíèçàöèè, ÷òîáû èñïîëüçîâàòü âàøó íîâóþ ïëàíåòó ';
+$LNG['extrafive'] = 'Êóïèòü ïëàíåòó çà %s DM';
+$LNG['error_3'] = 'Ó âàñ íåäîñòàòî÷íî òåìíîé ìàòåðèè!!';
